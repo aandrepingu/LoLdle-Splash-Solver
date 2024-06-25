636a612003e9e579864683365d5d80f9afcad6e0
aandrepingu <lungua0@gmail.com>
2024-06-25 11:41:20 -0400

    some main code + list of champion names

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+NUM_CHAMPIONS = 167
 
 class SplashCNN(nn.Module):
     def __init__(self, num_images, num_champs=167):
@@ -36,3 +37,25 @@ class SplashCNN(nn.Module):
     def predict(self, x):
         res = self.forward(x)
         # get champ with highest probability out of output from classifier
+
+def mapping(tokens):
+    
+    # Generates 2-way mapping of tokens (subgraphs) to numbers
+    token_to_id = {}
+    id_to_token = {}
+
+    for i, token in enumerate(set(tokens)):
+        token_to_id[token] = i
+        id_to_token[i] = token
+    return token_to_id, id_to_token
+
+if __name__ == "__main__":
+
+    with open('./champnames.txt', 'r') as file:
+        champ_names = file.readlines()
+
+    assert len(champ_names) == NUM_CHAMPIONS
+    champ_to_id, id_to_champ = mapping(champ_names)
+    del champ_names
+
+    # load images ferom splash directory
