5dba5d2497db36800e50d0c8239c5dfe7cc4d6c4
aandrepingu <lungua0@gmail.com>
2024-06-25 12:32:47 -0400

    wrong file

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ batch_size = 4
 lr = 0.001
 
 
+def mapping(tokens):
+    
+    # Generates 2-way mapping of tokens (subgraphs) to numbers
+    token_to_id = {}
+    id_to_token = {}
+
+    for i, token in enumerate(set(tokens)):
+        token_to_id[token] = i
+        id_to_token[i] = token
+    return token_to_id, id_to_token
+
+if __name__ == "__main__":
+
+    with open('./champnames.txt', 'r') as file:
+        champ_names = file.readlines()
+
+    assert len(champ_names) == NUM_CHAMPIONS
+    champ_to_id, id_to_champ = mapping(champ_names)
+    del champ_names
+
+    # load images ferom splash directory
--- model.py
+++ model.py
@@ -38,24 +38,3 @@ class SplashCNN(nn.Module):
         res = self.forward(x)
         # get champ with highest probability out of output from classifier
 
-def mapping(tokens):
-    
-    # Generates 2-way mapping of tokens (subgraphs) to numbers
-    token_to_id = {}
-    id_to_token = {}
-
-    for i, token in enumerate(set(tokens)):
-        token_to_id[token] = i
-        id_to_token[i] = token
-    return token_to_id, id_to_token
-
-if __name__ == "__main__":
-
-    with open('./champnames.txt', 'r') as file:
-        champ_names = file.readlines()
-
-    assert len(champ_names) == NUM_CHAMPIONS
-    champ_to_id, id_to_champ = mapping(champ_names)
-    del champ_names
-
-    # load images ferom splash directory
